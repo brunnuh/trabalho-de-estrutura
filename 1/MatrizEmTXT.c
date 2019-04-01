b2369b12afe102f7dd016f2b8512dbb31d1c3966
Brunno Santos <buhsantos17@gmail.com>
2019-04-01 11:04:22 -0300

    criando funcao propria de ler arquivo txt

--- 1/MatrizEmTXT.c
+++ 1/MatrizEmTXT.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+float *LerArqui(){
+	float *VetorM;
 	int m, n;
 	int i, j;
-	float *Ma;
 	FILE *ArquiMatrizA;//criando um ponteiro para um arquivo
 	ArquiMatrizA = fopen("ArquivoMatrizA.txt","r");//mostrando para o ponteiro seu caminho e o que é pra fazer, r -- ler
-	
 	if(ArquiMatrizA != NULL){
-		fscanf(ArquiMatrizA, "%i %i",&m,&n);
-		Ma = (float*)malloc(m*n*sizeof(float));
-		if(Ma != NULL){
+		fscanf(ArquiMatrizA, "%i %i",&m,&n);//lendo linhas e colunas
+		VetorM = (float*)malloc(m*n*sizeof(float));
+		if(VetorM != NULL){
 			for(i = 0; i < m; i++){
 				for(j = 0; j < n; j++){
 					fscanf(ArquiMatrizA, "%f",&Ma[m*i+j]);
 				}
 				fscanf(ArquiMatrizA, "\n");
 			}
-			
-			for(i = 0; i < m; i++){
-				for(j = 0; j < n; j++){
-					printf("\t%.0f ",Ma[m*i+j]);
-				}
-				printf("\n");
-			}
 		}
 		else{
-			printf("ERRO, alocacao da matriz Ma.");
-			exit(0);
+			printf("\nERRO, alocacao da matriz Ma.");
+			return NULL;//receber null e mata o programa
 		}
 	}
 	else{
 		printf("ERRO, arquivo nao encontrado.");
+		return NULL;
+	}
+	
+}
+
+int main(){
+	int m, n;
+	int i, j;
+	float *Ma;
+
+	
+	for(i = 0; i < m; i++){
+		for(j = 0; j < n; j++){
+			printf("\t%.0f ",Ma[m*i+j]);
+		}
+		printf("\n");
 	}
 	
 	
