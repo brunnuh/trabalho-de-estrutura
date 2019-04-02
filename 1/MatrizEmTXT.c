807927c571245805e67ff913a9c2fad1e9c47613
Brunno Santos <buhsantos17@gmail.com>
2019-04-02 09:09:29 -0300

    nada mudou

--- 1/MatrizEmTXT.c
+++ 1/MatrizEmTXT.c
@@ -3,9 +3,9 @@
 
 float *LerArqui(){
 	float *VetorM;
+	FILE *ArquiMatrizA;//criando um ponteiro para um arquivo
 	int m, n;
 	int i, j;
-	FILE *ArquiMatrizA;//criando um ponteiro para um arquivo
 	ArquiMatrizA = fopen("ArquivoMatrizA.txt","r");//mostrando para o ponteiro seu caminho e o que é pra fazer, r -- ler
 	if(ArquiMatrizA != NULL){
 		fscanf(ArquiMatrizA, "%i %i",&m,&n);//lendo linhas e colunas
@@ -13,10 +13,11 @@ float *LerArqui(){
 		if(VetorM != NULL){
 			for(i = 0; i < m; i++){
 				for(j = 0; j < n; j++){
-					fscanf(ArquiMatrizA, "%f",&Ma[m*i+j]);
+					fscanf(ArquiMatrizA, "%f",&VetorM[m*i+j]);
 				}
 				fscanf(ArquiMatrizA, "\n");
 			}
+			return VetorM,m,n;
 		}
 		else{
 			printf("\nERRO, alocacao da matriz Ma.");
@@ -34,7 +35,8 @@ int main(){
 	int m, n;
 	int i, j;
 	float *Ma;
-
+	
+	
 	
 	for(i = 0; i < m; i++){
 		for(j = 0; j < n; j++){
